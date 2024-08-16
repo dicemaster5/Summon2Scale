07d629ca6f3f9f591c5545c57b2e9971ed36fb75
Dice <dicemaster555@gmail.com>
2024-08-16 22:31:05 +0100

    Created a new block asset and follow cam

---
+++ systems/follow_cam.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@export var follow_target: Node2D
+@export var follow_speed: float = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = lerp(position, follow_target.global_position, follow_speed * delta)
