c538210638ea78cbb6b00de834f7a3e31762e5f2
alifeee <alifeeerenn@hotmail.com>
2024-08-17 14:23:25 +0100

    add global variable file

---
+++ systems/globals.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var we_are_in_a_menu: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
