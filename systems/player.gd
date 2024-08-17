da040a7fcba60466fe6cb97ad6248e60c0d72451
Dice <dicemaster555@gmail.com>
2024-08-17 14:33:19 +0100

    Coyote time yeehaw

--- systems/player.gd
+++ systems/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @export var walk_speed: float = 300.0
 @export var run_speed: float = 500.0
-@export var jump_velocity: float = -400.0
+@export var max_jump_velocity: float = 400.0
+@export var coyote_frames: int = 30  # How many in-air frames to allow jumping
 
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coyote_timer: Timer = $CoyoteTimer
@@ -10,15 +11,16 @@ extends CharacterBody2D
 var move_speed: float
 var jumped: bool
 var can_jump: bool
+var jump_force: float
 
-var coyote_frames = 6  # How many in-air frames to allow jumping
 var coyote = false  # Track whether we're in coyote time or not
 var last_floor = false  # Last frame's on-floor state
-
 var gravity_enabled = true
 
 func _ready() -> void:
 	animator.play()
+	coyote_timer.wait_time = coyote_frames / 60.0
+	print(coyote_timer.wait_time)
 
 func _physics_process(delta: float) -> void:
 	if Globals.we_are_in_a_menu:
@@ -31,11 +33,8 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	# Handle jump.
 	if is_on_floor():
-		can_jump = true
-		coyote_timer.stop()
-
+		jumped = false
 		if direction != 0:
 			if Input.is_action_pressed("run"):
 				move_speed = run_speed
@@ -44,11 +43,19 @@ func _physics_process(delta: float) -> void:
 				move_speed = walk_speed
 				animator.play("walk")
 		else:
-			animator.play("Idle")
+			animator.play("Idle") 
+
+	if !is_on_floor() and last_floor and !jumped:
+		coyote = true
+		coyote_timer.start()
+		print("Timer Started")
 
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
-		velocity.y = jump_velocity
+		velocity.y = -max_jump_velocity
 		jumped = true
+		coyote = false
+		coyote_timer.stop()
+		print("JUMPED!!!")
 
 	if direction:
 		velocity.x = direction * move_speed
@@ -61,5 +68,9 @@ func _physics_process(delta: float) -> void:
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, move_speed)
-
+	
+	last_floor = is_on_floor()
 	move_and_slide()
+
+func _on_coyote_timer_timeout() -> void:
+	coyote = false
