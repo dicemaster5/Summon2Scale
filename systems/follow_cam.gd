2df947d6812c6432493d7d770622a46d1981660c
Dice <dicemaster555@gmail.com>
2024-08-17 10:27:54 +0100

    Updated the gitignore

--- systems/follow_cam.gd
+++ systems/follow_cam.gd
@@ -10,11 +10,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = lerp(position, follow_target.global_position, follow_speed * delta)
-
-
-func _on_win_area_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_win_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
