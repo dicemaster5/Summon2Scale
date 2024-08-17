07ec5a7e9943ad3962ccda10652781d95e998f1b
Dice <dicemaster555@gmail.com>
2024-08-17 10:52:33 +0100

    Created win area for testing in main

--- systems/win_area.gd
+++ systems/win_area.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var win_screen_scene: PackedScene
+@export var ui_canvas: CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +13,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		var win_screen: Control = win_screen_scene.instantiate()
-		add_sibling(win_screen)
+		ui_canvas.add_child(win_screen)
 	pass # Replace with function body.
