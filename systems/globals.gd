7a961bfc98d3ab2e63c72175371348ac48d02a92
alifeee <alifeeerenn@hotmail.com>
2024-08-18 16:32:40 +0100

    fix game progession with menus and stuff

--- systems/globals.gd
+++ systems/globals.gd
@@ -17,23 +17,25 @@ var mouse_in_tower_area: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(0.1).timeout
 	change_gamemode(GAMEMODE.INTRO)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_BACKSPACE) || Input.is_key_pressed(KEY_ESCAPE):
+	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().reload_current_scene()
-		change_gamemode(GAMEMODE.START)
-		print("Game Restarted!")
+		await get_tree().create_timer(0.1).timeout
+		change_gamemode(GAMEMODE.INTRO)
 
 	# restart game in build mode
-	if Input.is_key_pressed(KEY_R):
+	if Input.is_key_pressed(KEY_R) and (current_gamemode == GAMEMODE.PLAYER or current_gamemode == GAMEMODE.BUILDER):
 		get_tree().reload_current_scene()
 		await get_tree().create_timer(0.01).timeout
 		call_deferred("change_gamemode", GAMEMODE.BUILDER)
 		print("build mode Restarted!")
 
 func change_gamemode(new_gamemode: GAMEMODE) -> void:
+	print("changed gamemode to ", new_gamemode)
 	current_gamemode = new_gamemode
 	gamemode_changed.emit(new_gamemode)
 
