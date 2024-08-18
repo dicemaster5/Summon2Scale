bb1aef952332445e8f077e839dbddaf7aaa228c7
alifeee <alifeeerenn@hotmail.com>
2024-08-18 16:08:03 +0100

    add ordinal to scoreboard

--- systems/scoreboardentry.gd
+++ systems/scoreboardentry.gd
@@ -1,5 +1,7 @@
 class_name ScoreboardEntry extends HBoxContainer
 
+@export var ordinal: String = "1."
+@export var ordinal_label: Label
 @export var username: String = "loading..."
 @export var username_label: Label
 @export var score: String = "loading..."
@@ -7,6 +9,7 @@ class_name ScoreboardEntry extends HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ordinal_label.text = ordinal
 	username_label.text = username
 	score_label.text = score
 	pass # Replace with function body.
