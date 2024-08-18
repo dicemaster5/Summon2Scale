83565c4d54b0261d77ed24b3cd7d0240eb19be13
Dice <dicemaster555@gmail.com>
2024-08-18 10:38:13 +0100

    new tower added :tokyo_tower:

--- scenes/block_container_area.gd
+++ scenes/block_container_area.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 
 func spawn_shape_in_position(pos: int):
 	var rand_block_id = randi_range(0,blocks.size()-1)
-	var position = positions[pos]
+	var place_position = positions[pos]
 	
 	var new_block = blocks[rand_block_id].instantiate()
-	new_block.global_position = position.global_position
+	new_block.global_position = place_position.global_position
 	new_block.initial_position = pos
 	new_block.scale = Vector2(0.1, 0.1)
 	
